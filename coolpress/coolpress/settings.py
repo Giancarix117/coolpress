0b5b39bbfdc90df6721497fe5b18df1d3560e627
Giancarix117 <56864380+Giancarix117@users.noreply.github.com>
2021-10-21 16:55:35 +0200

    admin setup and some things changed there

--- coolpress/coolpress/settings.py
+++ coolpress/coolpress/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'coolpress.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'dpk_db.sqlite3',
     }
 }
 
--- coolpress/press/models.py
+++ coolpress/press/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.db import models
+
 from enum import Enum
 
 from django.contrib.auth.models import User
